1ed0c1d01885dc542400a0e5b6d2fdfab7f6c270
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-03-13 00:23:48 +0100

    style changes

--- src/app.jsx
+++ src/app.jsx
@@ -24,13 +24,14 @@ export default class App extends Component {
     return (
       <div className='container'>
         <div className='jumbotron'>
-          <h1>San Diego Top Spots</h1>
+        <h1><img src="./img/words.png" alt="San Diego Top Spots" style={{ width: '30%', height: 'auto' }} /></h1>
           <p>A list of the top 30 places to see in San Diego, California.</p>
           <div className="background-images">
             <div style={{ backgroundImage: "url('./img/bet.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '200px', width: '33.333%' }}></div>
             <div style={{ backgroundImage: "url('./img/arch.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '200px', width: '33.333%' }}></div>
             <div style={{ backgroundImage: "url('./img/row.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '200px', width: '33.333%' }}></div>
           </div>
+          <div className="seal"></div>
         </div>
         {this.state.topspots ? this.state.topspots.map((topspot) => (
           <TopSpot
